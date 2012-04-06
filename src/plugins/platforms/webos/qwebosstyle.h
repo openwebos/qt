75eca7f3895fa0eb2b732d2f0b3a9243a54a7677
Kuldeep Modi <kuldeep.modi@palm.com>
2012-04-05 17:35:37 -0700

    Deleted Unnecessary files.
    Change-Id: Idc15894c98975e9c9aa14d0ab61855fd34c02ff8

--- src/plugins/platforms/webos/qwebosstyle.h
+++
@@ -1,9 +0,0 @@
-#include <QStyle>
-#include <QCommonStyle>
-#include <QPlastiqueStyle>
-
-class QWebOSStyle : public QPlastiqueStyle
-{
-	public:
-		int styleHint(StyleHint hint, const QStyleOption* option = 0, const QWidget* widget = 0, QStyleHintReturn* returnData = 0) const;
-};
